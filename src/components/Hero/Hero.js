733a174364076b1c1c7bb51595f5fc40f7757ea5
Adnaan Hussain <adnaan137@gmail.com>
2023-09-24 18:26:27 +0530

    Updated Projects

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Portfolio
+        </SectionTitle>
+        <SectionText>
+        I like complex problems and I love simple solutions. So in 2021, I took a life altering decision to follow my Passion for Programming, Fast forward to 2023, <br />
+        I am a ReactJs/NextJs Developer. <br />
+       Now that you know, <br />
+        Check out my Projects.
+        </SectionText>
+        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    title: "Nemesis AI",
+    description:
+      "Using Next.JS, Tailwind, Prisma, Stripe, Open Ai and Crisp for Customer Support, I built a SAAS Platform, Where users get Conversation AI and many more features. Go check it out.",
+    image: "/images/NAI.png",
+    tags: ["NextJs", "Prisma", "React", "OpenAI"],
+    source: "https://nemesisai.vercel.app",
+    visit:
+      "https://mega.nz/file/1zBB0BhI#wK8n3rYKwqXTL5QDTTUyAuA6cvOMOngCjxdb-Uzsc14",
+    id: 0,
+  },
+  {
+    title: "TalkItOut",
+    description:
+      "Our messaging app, inspired by WhatsApp, is designed to let users send and receive messages between the client and server side. In Phase 1, we focused on developing the client side of the app, which includes key functionalities such as sending and receiving messages. ",
+    image: "/images/talk.png",
+    tags: ["React", "Firebase", "SCSS"],
+    source: "https://talkitoutchat.netlify.app/register",
+    visit: "https://github.com/Adnaanlegend/TalkItOut",
+    id: 1,
+  },
+  {
+    title: "Youtoob",
+    description:
+      "Built a Youtube Clone to understand the structure of a Huge User Base Applications. This is 100% Mobile Responsive and without Ads , Try it out!",
+    image: "/images/yt.png",
+    tags: ["React", "TailwindCSS", "RapidApi"],
+    source: "https://youtoob.netlify.app",
+    visit: "https://github.com/Adnaanlegend/Youtoob",
+    id: 2,
+  },
+  {
+    title: "Basket Dashboard",
+    description:
+      "Built with React for dynamic interactivity, TailwindCSS  sleek styling, and enhanced with Apex Charts and Framer Motion for data visualization and animations.",
+    image: "/images/Dash.png",
+    tags: ["React", "TailwindCSS", "Apex Charts", "Framer Motion"],
+    source: "https://github.com/Adnaanlegend/Basket",
+    visit: "https://takeabasket.netlify.app/",
+    id: 3,
+  },
+  {
+    title: "Gallery App AI",
+    description:
+      "It was my first Next.JS Project and I tried to make it with cloudinary and Shadcn-ui, There are a lot of features.Give it a go.",
+    image: "/images/gai.png",
+    tags: ["Next.JS", "Cloudinary", "ShadcnUI"],
+    source: "https://github.com/Adnaanlegend/galleryapp",
+    visit: "https://galleryappai.vercel.app/gallery",
+    id: 3,
+  },
+  {
+    title: "Social Networking Site",
+    description: "Just trying to do something.UNIQUE",
+    image: "/images/cs.png",
+    tags: ["NextJS", "Tap to know more", "Clerk"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "October - Started my journey" },
+  { year: 2021, text: "Learnt everything from Youtube, Webinars, Masterclass" },
+  { year: 2022, text: "September- Started working as a Freelance developer" },
+  { year: 2022, text: "October- Made my First Full Stack Application" },
+  { year: 2023, text: "Full-Stack Developer" },
+];
